4258926267ab123982ec10a0c84bc3b76cb52510
MyKnu <k1alty@naver.com>
2024-05-15 18:01:39 +0900

    update alchemist

--- cp2kbrew/_main.py
+++ cp2kbrew/_main.py
@@ -6,6 +6,25 @@ class Alchemist(Opener):
     def __init__(self, logfile: str, trjfile: str = None, *, trjfmt: str = "auto", mode: str = None) -> None:
         super().__init__(logfile, trjfile, trjfmt=trjfmt, mode=mode)
 
+    def __repr__(self) -> str:
+        status = ""
+        status += "=" * 25 + "\n"
+        status += f"{'Opener':^25s}\n"
+        status += "=" * 25 + "\n"
+        status += f"nFrame : {self.nframe}\n"
+        status += f"nCell  : {self.cell.shape}\n"
+        status += f"nCoord : {self.coord.shape}\n"
+        status += f"nEnergy: {self.energy.shape}\n"
+        status += f"nStress: {self.stress.shape}\n"
+        status += f"nVirial: {self.virial.shape}\n"
+        status += "=" * 25 + "\n"
+        status += "=" * 25 + "\n"
+        status += f"{'Doctor':^25s}\n"
+        status += "=" * 25 + "\n"
+        status += "\n".join([f"{k.upper()} = {v}" for k, v in self.doctor.check().items()]) + "\n"
+        status += "=" * 25 + "\n"
+        return status
+
     @property
     def doctor(self):
         return Doctor(opener=self)
